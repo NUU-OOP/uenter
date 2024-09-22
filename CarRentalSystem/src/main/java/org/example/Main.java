83f03937d061bdc4c1a35c01332e456013dd7733
behruz-cell <bekhruz8808@gmail.com>
2024-09-22 03:21:23 -0700

    V2

--- CarRentalSystem/src/main/java/org/example/Main.java
+++ CarRentalSystem/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main File");
+        System.out.println("Main File" + "\n 1.Show cars" + "\n 2.Show admin" +
+                "\n 3.Show manager" + "\n 4.Show staff" + "\n 5.Show customer" + "\n 6.Show driver");
     }
 }
--- CarRentalSystem/src/main/java/org/example/user1/user1.java
+++ CarRentalSystem/src/main/java/org/example/user1/user1.java
@@ -1,5 +1,13 @@
 package org.example.user1;
 
 public class user1 {
-    private String M;
-}
+    private int userId;
+    private String password;
+
+    public void login(){
+
+    }
+
+    public void logout(){
+
+    }}
