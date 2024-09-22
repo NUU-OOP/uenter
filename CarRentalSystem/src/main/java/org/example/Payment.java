3dadbab928ceb637971eb8758a87e088479a983a
user <user@WIN-BRLSMQOVQ3V>
2024-09-22 15:23:53 +0500

    V2

---
+++ CarRentalSystem/src/main/java/org/example/Payment.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Date;
+
+public class Payment {
+    double amount;
+    Date date;
+    public void processPayment(){
+
+    }
+    public void generateInvoice(){
+
+    }
+}
